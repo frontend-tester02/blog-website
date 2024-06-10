b1d948d6436955488df379b7f6b1383130bad3bd
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-06-10 22:17:44 +0900

    Updated blog page, add some class names

--- blog.php
+++ blog.php
@@ -32,20 +32,31 @@
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                     echo "<li class='blog__item'>";
-                    echo "<img src='./images/uploads/" . $row["image"] . "' alt='Post Image' style='width: 382px; height: 338px;'>";
-                    echo "<h2>" . $row["title"] . "</h2>";
+                    echo "<img src='./images/uploads/" . $row["image"] . "' class='post-img' alt='Post Image' style='width: 382px; height: 338px;'>";
+                    echo "<div class='blog__item-content'>";
+                    echo "<h2 class='blog__item-title'>" . $row["title"] . "</h2>";
                     echo "<p class='blog__post' >" . $row["body"] . "</p>";
                     echo "<div class='blog__data'>";
                     echo "<div class='blog__btns'>";
                     echo "<button type='button' class='blog__btn blog__edit-btn' onclick=\"location.href='edit-post.php?post_id=" . $row["id"] . "'\">Edit</button>";
                     echo "<button type='button' class='blog__btn blog__view-btn' onclick=\"if(confirm('Are you sure you want to delete this post?')) { location.href='delete-post.php?post_id=" . $row["id"] . "'; }\">Delete</button>";
+                    echo "<button type='button' class='blog__btn blog__like-btn' data-post-id='" . $row["id"] . "'>Like</button>";
+                    echo "<button type='button' class='blog__btn blog__comment-btn' data-post-id='" . $row["id"] . "'>Comment</button>";
                     echo "</div>";
                     echo "<span class='blog__time'>" . date('F j, Y', strtotime($row['created_at'])) . "</span>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "<form action='#' class='comment-form'>";
+                    echo "<div class='form__group'>";
+                    echo "<input class='form__field comment-input' placeholder='Write Comment...'>";
+                    echo "</div>";
+                    echo "</form>";
+                    echo "<div class='comment-text'>";
                     echo "</div>";
                     echo "</li>";
                 }
             } else {
-                echo "0 results";
+                echo "There is no post";
             }
             $conn->close();
             ?>
