824e238d3d2cc9880d35170054e6c98c56d094e6
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-06-14 22:56:45 +0900

    Seperatte footer main page

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+  <footer class="site-footer">
+      <div class="site-footer__content">
+        <p>&copy 2024 Copyright by Nuriddinov Shokhrukh</p>
+      </div>
+    </footer>
+
+    <script src="js/main.js?v=1.0"></script>
+  </body>
+  </html>
